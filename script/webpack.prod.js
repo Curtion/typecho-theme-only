039516349ebe64fe2ca346b8f2196bb376dd191d
Curtion <curtion@126.com>
2020-07-02 23:12:47 +0800

    config

--- script/webpack.prod.js
+++ script/webpack.prod.js
@@ -1,9 +1,3 @@
-const path = require('path')
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, '../', './src/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../', './dist')
-  }
+  mode: 'production'
 }
