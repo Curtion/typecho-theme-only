caf5d1a478694ee2e9c24c5426e81b82a1d8e3f8
Curtion <curtion@126.com>
2021-03-12 23:21:45 +0800

    React 初始化

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>在React中使用React Router v6 的指南</p>
+    </div>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
-    <div>测试</div>
-  </React.StrictMode>,
+    <Router>
+      <button path="/a" element={<Home />} />
+    </Router>
+  </React.StrictMode >,
   document.getElementById('root')
 );
