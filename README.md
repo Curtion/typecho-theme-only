f4cf2bc56d1d9ddd83be696fa6f91ad9fb62944a
Curtion <curtion@126.com>
2021-03-12 22:00:35 +0800

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-typecho 主题，正在开发，搭配 https://github.com/moefront/typecho-plugin-Restful 使用
--- script/webpack.prod.js
+++
@@ -1,5 +0,0 @@
-const merge = require('webpack-merge')
-const base = require('./webpack.base.js')
-module.exports = merge(base, {
-  mode: 'production'
-})
